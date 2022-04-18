407a5cf013c59b894f87f461cc3de94fdc434b0d
Nathan Capiaux <capiauxnathan@gmail.com>
2022-04-18 20:00:53 +0200

    Implement basic Instructions

--- core/chip8.go
+++ core/chip8.go
@@ -3,6 +3,7 @@ package core
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 )
 
@@ -55,7 +56,6 @@ func Init() Chip8 {
 	return chip8
 }
 
-// TODO: Open Rom file and copy content to memory
 func (c *Chip8) LoadRom(romPath string) error {
 	rom, err := os.Open(romPath)
 
@@ -87,5 +87,101 @@ func (c *Chip8) LoadRom(romPath string) error {
 		return readErr
 	} else {
 		return nil
+	}
+}
+
+// TODO
+func (c *Chip8) readInstruction(opcode uint16) error {
+	switch opcode & 0xF000 {
+	case 0x0000:
+
+	case 0x1000:
+		// JMP Instruction 0x1nnn - Jump to adress nnn
+		nextPc := opcode & 0x0FFF
+		c.pc = nextPc
+	case 0x2000:
+		// Call subroutine at nnn.
+		// The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
+		c.sp++
+		c.stack[c.sp] = c.pc
+		c.pc = opcode & 0x0FFF
+	case 0x3000:
+		// Skip next instruction if Vx = kk.
+		// The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
+		// Skip next instruction if Vx = kk.
+		// The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
+		kk := uint8(opcode & 0x00FF)
+		x := (opcode & 0x0F00) >> 8
+		if c.vx[x] == kk {
+			c.pc += 2
+		}
+	case 0x4000:
+		// Skip next instruction if Vx != kk.
+		// The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
+		kk := uint8(opcode & 0x00FF)
+		x := (opcode & 0x0F00) >> 8
+		if c.vx[x] != kk {
+			c.pc += 2
+		}
+	case 0x5000:
+		// 0x5xy0
+		// Skip next instruction if Vx = Vy.
+		// The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
+		x := (opcode & 0x0F00) >> 8
+		y := (opcode & 0x00F0) >> 4
+		if c.vx[x] != c.vx[y] {
+			c.pc += 2
+		}
+	case 0x6000:
+		// Ox6xkk
+		// Set Vx = kk.
+		// The interpreter puts the value kk into register Vx.
+		x := (opcode & 0x0F00) >> 8
+		kk := uint8(opcode & 0x00FF)
+		c.vx[x] = kk
+	case 0x7000:
+		// 0x7xkk
+		// Set Vx = Vx + kk.
+		// Adds the value kk to the value of register Vx, then stores the result in Vx.
+		x := (opcode & 0x0F00) >> 8
+		kk := uint8(opcode & 0x00FF)
+		c.vx[x] = c.vx[x] + kk
+	case 0x8000:
+	case 0x9000:
+		// 0x9xy0
+		// Skip next instruction if Vx != Vy.
+		// The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
+		x := (opcode & 0x0F00) >> 8
+		y := (opcode & 0x00F0) >> 4
+
+		if c.vx[x] != c.vx[y] {
+			c.pc += 2
+		}
+	case 0xA000:
+		// OxAnnn
+		// Set I = nnn.
+		// The value of register I is set to nnn.
+		nnn := opcode & 0x0FFF
+		c.i = nnn
+	case 0xB000:
+		// 0xBnnn
+		// Jump to location nnn + V0.
+		// The program counter is set to nnn plus the value of V0.
+		nnn := opcode & 0x0FFF
+		v0 := c.vx[0]
+		c.pc = nnn + uint16(v0)
+	case 0xC000:
+		// Cxkk - RND Vx, byte
+		// Set Vx = random byte AND kk.
+		// The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx
+		x := (opcode & 0x0F00) >> 8
+		kk := uint8(opcode & 0x00FF)
+		randomValue := uint8(rand.Intn(256)) & kk
+		c.vx[x] = randomValue
+	case 0xD000:
+	case 0xE000:
+	case 0xF000:
+	default:
+		return fmt.Errorf("Unknown Opcode")
 	}
 }
