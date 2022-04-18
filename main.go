9387aaee327fe0639b4e7b6402934d84c003a766
Nathan Capiaux <capiauxnathan@gmail.com>
2022-04-18 18:33:04 +0200

    change main to private

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/Nactik/Chip8Go/core"
 )
 
-func Main() {
+func main() {
+
+	if len(os.Args) == 1 {
+		fmt.Println("Missing argument : Rom path")
+		os.Exit(-1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+		os.Exit(-1)
+	}
+
+	romPath := os.Args[1]
 	chip8 := core.Init()
+
+	chip8.LoadRom(romPath)
+
 }
